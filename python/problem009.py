898f3d87d43c496b4cfb71ea9c2d7a686443a6e8
Francesco Pezzella <franzpezzella@gmail.com>
2014-01-12 16:56:19 +0100

    Make PEP8 compliant.

--- python/problem009.py
+++ python/problem009.py
@@ -1,16 +1,27 @@
-def specialTriplet(n): 
-    assert type(n) == int  
-    upperBound = n / 2   # (a + b + c) / 2 is always bigger than max(a, b, c)
-    result = None
-    for a in range(upperBound, 1, -1):
+def find_abc_product(abc_sum):
+    """
+    abc_sum is (a + b + c) where a, b, c are any positive integers
+    Returns (a * b * c) if abc_sum represents the sum of a pythagorean triplet
+    """ 
+    assert type(abc_sum) == int and abc_sum > 0
+    # given a pythagorean triplet (a, b, c)
+    # (a + b + c) / 2 is always bigger than max(a, b, c)
+    # hence start the outer loop from n / 2
+    upper_bound = abc_sum / 2   
+    triplet_product = None
+    for a in range(upper_bound, 1, -1):
         for b in range(a - 1, 1, -1):
+            # get the third term for the triplet
             c = (a**2 - b**2)**0.5
-            if  a + b + c == n:
-                result = int(reduce(lambda x, y: x*y, [a, b, c]))
+            # if the pythagorean triplet match the input n, compute the product
+            if  a + b + c == abc_sum:
+                triplet_product = int(reduce(lambda x, y: x*y, [a, b, c]))
                 x, y, z = b, c, a
-    if result:
-        return "Triplet (%d + %d + %d) = %d, Product (a*b*c) = %d" %(x, y, z, n, result)
+    if triplet_product:
+        return "Pythagorean Triplet (%d + %d + %d) = %d\nProduct (a * b * c) = %d"     \
+                 %(x, y, z, abc_sum, triplet_product)
     else:
-        return "Cannot find a triplet (a + b + c) = %d" % n
+        return "Cannot find a triplet (a + b + c) = %d" % abc_sum
         
-print specialTriplet(1000) 
+print find_abc_product(1000) 
+
