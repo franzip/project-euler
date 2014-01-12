60cbc0d55c9ceb333293401bae11d5d196dcb0a4
Francesco Pezzella <franzpezzella@gmail.com>
2014-01-12 14:28:14 +0100

    Make PEP8 compliant.

--- python/problem002.py
+++ python/problem002.py
@@ -1,15 +1,15 @@
-def fib(upperBound):
-    assert type(upperBound) == int or type(upperBound) == long
-    sumEvenFibo = 0
+def sum_even_fibo(upper_bound):
+    assert type(upper_bound) == int or type(upper_bound) == long
+    add_fibo = 0
     fibo, increment = 1, 0
-    while fibo < upperBound:
-        # generate fib numbers on the fly
+    # generate fib numbers on the fly from 0 to upper_bound
+    while fibo < upper_bound: 
         fibo, increment = fibo + increment, fibo
         if fibo % 2 == 0:
-            sumEvenFibo += fibo
-    return sumEvenFibo
+            add_fibo += fibo
+    return add_fibo
     
-print fib(4000000)
+print sum_even_fibo(4000000)
         
         
 
--- python/problem003.py
+++ python/problem003.py
@@ -1,13 +1,16 @@
-def largestPrime(n):
-    assert type(n) == int or type(n) == long
-    def helper(n, divisor):
-        while divisor != n:
-            if n % divisor == 0:
-                n /= divisor
+def find_largest_factor(prime):
+    assert type(prime) == int or type(prime) == long
+    # helper function
+    def helper(prime, divisor):
+        # check for divisors starting from 2
+        while prime != divisor:
+            if prime % divisor == 0:
+                prime /= divisor
             else:
                 divisor += 1
         return divisor
-    return helper(n, 2)
+    # seed helper with 2    
+    return helper(prime, 2)
     
     
-print largestPrime(600851475143)
+print find_largest_factor(600851475143)
