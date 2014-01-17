c812c63acb163620bf77646228554a8ceb9b011a
Francesco Pezzella <franzpezzella@gmail.com>
2014-01-17 16:28:11 +0100

    Initial commit.

---
+++ python/problem015.py
@@ -0,0 +1,11 @@
+def lattice_paths(n_grid):
+    assert type(n_grid) == int and n_grid > 0
+    return factorial(2 * n_grid) / factorial(n_grid)**2
+    
+def factorial(n):
+    if n < 1:
+        return 1
+    return n * factorial(n - 1)
+
+            
+print lattice_paths(3)
