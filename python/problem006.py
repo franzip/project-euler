755339ce3d1b3bb35bf917c24f989f5c304c02eb
Francesco Pezzella <franzpezzella@gmail.com>
2014-01-23 18:59:06 +0100

    Fix indentation.

--- python/problem006.py
+++ python/problem006.py
@@ -2,6 +2,6 @@ def sum_square_diff(n):
     assert n >= 2 and type(n) == int
     # return (1 + 2 + ... + n)^2 - (1^2 + 2^2 + ... + n^2)
     return sum([x for x in range(1, n + 1)]) ** 2 -     \
-            sum([x ** 2 for x in range(1, n + 1)]) 
+           sum([x ** 2 for x in range(1, n + 1)]) 
     
 print sum_square_diff(100)
