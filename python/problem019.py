29460ee5147fab1e3d85d6ac4bdc8da8a139b24f
Francesco Pezzella <franzpezzella@gmail.com>
2014-03-09 15:04:54 +0100

    Fix comments.

--- python/problem019.py
+++ python/problem019.py
@@ -4,25 +4,26 @@ def is_leap(year):
     return False
 
 def count_sundays(start_year, end_year):
-    # months[i][1] maps for leap years, months[i][0] for non leap years
+    # months[i][1] maps months length for leap years, months[i][0] for non leap years
     months = {1:(31, 31), 2:(28, 29), 3:(31, 31), 4:(30, 30), 5:(31, 31), 6:(30, 30),
-    7:(31, 31), 8:(31, 31), 9:(30, 30), 10:(31, 31), 11:(30, 30), 12:(31, 31)}
-    
+    7:(31, 31), 8:(31, 31), 9:(30, 30), 10:(31, 31), 11:(30, 30), 12:(31, 31)}  
     # week_day maps numbers 0 ... 6 to week days
     # 0 = Monday ... 6 = Sunday
     week_day = 0 
     matching_sundays = 0
     for years in range(start_year, end_year + 1):
         for month in range(1, len(months) + 1):
-            leap_year = is_leap(years)
+            leap_year = is_leap(years)         
             if not leap_year:
+                # access months[i][0] if not leap year
                 for day in range(1, months[month][0] + 1):
                     # increase week_day keeping it in range(0, 7)
                     week_day = (week_day + 1) % 7
-                    # First month day on Sunday (week_day = 6)
+                    # first month day on Sunday (week_day = 6)
                     if week_day == 6 and day == 1:
-                        matching_sundays += 1
+                        matching_sundays += 1       
             else:            
+                # access months[i][1] if leap year
                 for day in range(1, months[month][1] + 1):
                     week_day = (week_day + 1) % 7
                     if week_day == 6 and day == 1:
