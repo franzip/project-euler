4157173cfe32dc21e1489e1617b84b86f2abcbb7
Francesco Pezzella <franzpezzella@gmail.com>
2014-05-06 23:05:22 +0200

    Fix ident.

--- ruby/problem003.rb
+++ ruby/problem003.rb
@@ -1,9 +1,9 @@
 require 'prime'
 
 def largest_prime_factor(n)
-	raise ArgumentError unless n.is_a? Integer and n > 1
-	# get the largest prime that evenly divides n
-	Prime.each((n**0.5) + 1).select{ |x| n % x == 0 }.max
+  raise ArgumentError unless n.is_a? Integer and n > 1
+  # get the largest prime that evenly divides n
+  Prime.each((n**0.5) + 1).select{ |x| n % x == 0 }.max
 end
 
 puts largest_prime_factor(600851475143)
