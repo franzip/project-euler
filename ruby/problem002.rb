682b36feb93f1a7970a97f5fed567c488f558828
Francesco Pezzella <franzpezzella@gmail.com>
2014-05-06 23:04:55 +0200

    Fix ident.

--- ruby/problem002.rb
+++ ruby/problem002.rb
@@ -1,18 +1,18 @@
 def fib_gen(limit)
-	# lazy fibonacci generator
+  # lazy fibonacci generator
   Enumerator.new do |fib|
-  	x, y = 0, 1
-  	while x < limit
- 			x, y = y, x + y	
- 			fib.yield x
-  	end
+    x, y = 0, 1
+    while x < limit
+      x, y = y, x + y	
+      fib.yield x
+    end
   end
 end
 
 def even_fib(upper_bound)
-	raise ArgumentError unless upper_bound.is_a? Fixnum and upper_bound > 1
-	# only catch even fibs in range
-	fib_gen(upper_bound).select { |x| x % 2 == 0 }.reduce(:+)
+  raise ArgumentError unless upper_bound.is_a? Fixnum and upper_bound > 1
+  # only catch even fibs in range
+  fib_gen(upper_bound).select { |x| x % 2 == 0 }.reduce(:+)
 end
 
 puts even_fib(4_000_000) 
