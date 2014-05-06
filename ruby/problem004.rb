9b2c6496d5b62bdc5e314cc21db6cd774337b3fc
Francesco Pezzella <franzpezzella@gmail.com>
2014-05-06 23:05:49 +0200

    Fix ident.

--- ruby/problem004.rb
+++ ruby/problem004.rb
@@ -1,12 +1,12 @@
 def is_palindrome?(s)
-	s == s.reverse
+  s == s.reverse
 end
 
 def largest_palindrome(digits)
-	raise ArgumentError unless digits.is_a? Fixnum and digits > 0
-	# set up and low bounds
-	up = (10**digits) - 1
-	low = up - (up / 10) + 1
+  raise ArgumentError unless digits.is_a? Fixnum and digits > 0
+  # set up and low bounds
+  up = (10**digits) - 1
+  low = up - (up / 10) + 1
   # loop from top to bottom and return on the first palindrome
   up.downto(low) do |x|
     up.downto(low) do |y|
