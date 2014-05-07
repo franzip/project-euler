ad69ce1c373f81ab000481f5d9d80ee7148a35b9
Francesco Pezzella <franzpezzella@gmail.com>
2014-05-08 00:23:38 +0200

    Fix ident.

--- ruby/problem006.rb
+++ ruby/problem006.rb
@@ -1,7 +1,7 @@
 def sum_square_diff(to_n)
-	raise ArgumentError unless to_n.is_a? Fixnum and to_n > 0
-	# (1 + 2 + ... + to_n)**2 - (1**2 + 2**2 + ... + to_n**2)
-	(1..to_n).reduce(:+)**2 - (1..to_n).map { |x| x**2 }.reduce(:+)
+  raise ArgumentError unless to_n.is_a? Fixnum and to_n > 0
+  # (1 + 2 + ... + to_n)**2 - (1**2 + 2**2 + ... + to_n**2)
+  (1..to_n).reduce(:+)**2 - (1..to_n).map { |x| x**2 }.reduce(:+)
 end
 
 puts sum_square_diff(100)
