002aeb7bcffe3fe9d783b0e55007fede8cc325f8
Francesco Pezzella <franzpezzella@gmail.com>
2014-05-12 22:41:44 +0200

    Fix indent.

--- ruby/problem016.rb
+++ ruby/problem016.rb
@@ -1,6 +1,6 @@
 def digit_sum(n)
-	raise ArgumentError unless n.is_a? Integer and n > 0
-	n.to_s.split("").map{ |x| x.to_i }.reduce(:+)
+  raise ArgumentError unless n.is_a? Integer and n > 0
+  n.to_s.split("").map{ |x| x.to_i }.reduce(:+)
 end
 
 p digit_sum(2**1000)
